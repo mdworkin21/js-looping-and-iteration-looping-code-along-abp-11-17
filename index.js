359a78f8f496e9a048703fceaaa4dd004a7c1d05
Matthew Dworkin <mdworkin21@gmail.com>
2017-11-10 18:39:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+function printBadges(array){
+  for (let i = 0; i < array.length; i++){
+    console.log(`Welcome ${array[i]}! You are employee #${[i + 1]}.`)
+  }
+  return array
+}
+
+
+function tailsNeverFails(){
+  function tails (){
+    return Math.random()}
+
+  while(tails() >= .5){
+    let count = 1
+    count++
+    return `You got ${count} tails in a row!`
+  }
+}
