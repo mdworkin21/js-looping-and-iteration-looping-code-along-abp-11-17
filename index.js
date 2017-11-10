6804dd0ee4232a5f5277f364cbd02ed27f52597d
Matthew Dworkin <mdworkin21@gmail.com>
2017-11-10 18:48:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function tailsNeverFails(){
     function tails(){
        Math.random() <= 0.5}
 
-       let i = 8
+       let i = 7
 
     while(tails()){
         return `You got ${i} tails in a row!`
